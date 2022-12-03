4ada4141ba8ddc9545cb0118b2d5e201b37a7077
KaPlaneta <planeta.karolinaa@gmail.com>
2022-12-03 12:20:54 +0100

    feat: use button library, picker library

--- FirstScreen.js
+++ FirstScreen.js
@@ -1,90 +1,82 @@
+import React from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Button,
+  SafeAreaView,
+  Text,
+  Alert,
+  TouchableOpacity,
+  Image,
+  Stack,
+  ScreenComponent,
+} from "react-native";
+import Logo from "./assets/logo.svg";
+import { StatusBar } from "expo-status-bar";
+import { ThemedButton } from "react-native-really-awesome-button";
 
- import React from 'react';
- import {
-   ScrollView,
-   StyleSheet,
-   useColorScheme,
-   View,
-   Button,
-   SafeAreaView,
-   Text, 
-   Alert, 
-   TouchableOpacity,
-   Image
- } from 'react-native';
- import Logo from "./assets/logo.svg";
- import { StatusBar } from "expo-status-bar";
-
-
-
-
-//  const AppButton = ({ onPress, title }) => (
-//   <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
-//     <Text style={styles.appButtonText}>{title}</Text>
-//   </TouchableOpacity>
-// );
-
-function FirstScreen({ navigation })
-{
-  return(
+function FirstScreen({ navigation }) {
+  return (
     <SafeAreaView style={styles.container}>
-     <Logo />
-     <StatusBar style="auto" />
-    <View style={styles.appButtonContainer}>
+      <Logo />
+      <StatusBar style="auto" />
+      {/* <View style={styles.appButtonContainer}> */}
+      {/* <AwesomeButton
+        CREATE
+        SONG
 
-    <Button
-    title="CREATE SONG"
-    onPress={() =>
-      navigation.navigate('SecondScreen',{
-        itemId: 86,
-        otherParam: 'anything you want here'})}
-    color="#fcf7fb"
-    
-
-    />
-
-  </View>
-  </SafeAreaView>
-
+        // onPress={() => navigation.navigate("SecondScreen")}
+        // color="#fcf7fb"
+      />
+      </View> */}
+      <ThemedButton
+        name="bruce"
+        // type="danger"
+        activityColor="#fffffff"
+        // textColor="a82f7a"
+        style={styles.button}
+        onPress={() => navigation.navigate("SecondScreen")}
+      >
+        CREATE SONG
+      </ThemedButton>
+    </SafeAreaView>
   );
-
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-          flex: 1,
-           justifyContent: 'center',  //na srodku
-           alignItems: 'center',   //tez
-           backgroundColor: '#f0f5f4',
-           flexDirection: 'column',
-           alignItems: "center",
-           justifyContent: "center",
-            },
-            title: {
-              textAlign: 'center',
-              marginVertical: 8,
-            },
-            appButtonContainer: {
-              elevation: 8,
-              backgroundColor: "#a82f7a",
-              borderRadius: 10,
-              paddingVertical: 10,
-              paddingHorizontal: 12
-            },
-            appButtonText: {
-              fontSize: 18,
-              color: "#fcf7fb",
-              fontWeight: "bold",
-              alignSelf: "center",
-              textTransform: "uppercase"
-            }
-            
-          });  
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f5f4",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    marginVertical: 8,
+  },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#a82f7a",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#fcf7fb",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
+  },
+  button: {
+    marginBottom: 16,
+    color: "#a82f7a",
+  },
+});
 
-      
-      
-  export default FirstScreen;
-  
-{/* <Text>{returnTheSame("siu")}</Text> */}
-    {/* <Text>{concatenateArray(["dffd","gfddffddf","hfdfd"])}</Text> */}
+export default FirstScreen;
--- SecondScreen.js
+++ SecondScreen.js
@@ -1,48 +1,147 @@
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+} from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { PickerIOS } from "@react-native-picker/picker";
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
-   Button,
- } from 'react-native';
+function SecondScreen({ route, navigation }) {
+  const [selectedValue, setSelectedValue] = useState("Metrum");
+  const [durMol, setdurMol] = React.useState("dur");
+  const [tonacja, settonacja] = React.useState(7);
+  const make = musicTheory[durMol];
+  const selectionString = make.name + " " + make.models[tonacja];
+  return (
+    <SafeAreaView style={styles.main}>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text>Please choose metrum:</Text>
+          <Picker
+            selectedValue={selectedValue}
+            style={{ width: 150 }}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedValue(itemValue)
+            }
+          >
+            <Picker.Item label="1/4" value="1/4" />
+            <Picker.Item label="2/4" value="2/4" />
+            <Picker.Item label="3/4" value="3/4" />
+            <Picker.Item label="4/4" value="4/4" />
+          </Picker>
 
-
- 
- function SecondScreen({navigation})
-{
-  return(
-    <View style={styles.container}>
-    {/* <Text>{returnTheSame("siu")}</Text> */}
-    {/* <Text>{concatenateArray(["dffd","gfddffddf","hfdfd"])}</Text> */}
-
-    <Button
-    onPress={() =>
-        navigation.navigate('ThirdScreen')}
-    title="START"
-    color="#841584"
-    />
-
-  </View>);
-
-  
-
+          <View>
+            <Text>Please choose major minor mode:</Text>
+            <Picker
+              selectedValue={durMol}
+              onValueChange={(value) => {
+                setdurMol(value);
+                settonacja(0);
+              }}
+            >
+              {Object.keys(musicTheory).map((value) => (
+                <Picker.Item
+                  key={value}
+                  value={value}
+                  label={musicTheory[value].name}
+                />
+              ))}
+            </Picker>
+            <Text>Please choose a key of {make.name}:</Text>
+            <Picker
+              selectedValue={tonacja}
+              key={durMol}
+              onValueChange={(value) => settonacja(value)}
+            >
+              {musicTheory[durMol].models.map((modelName, value) => (
+                <Picker.Item
+                  key={`${durMol}_${value}`}
+                  value={value}
+                  label={modelName}
+                />
+              ))}
+            </Picker>
+            <Text>You selected: {selectionString}</Text>
+          </View>
+          <Button
+            onPress={() =>
+              navigation.navigate("ThirdScreen", {
+                tonacja: make.models[tonacja],
+              })
+            }
+            title="START"
+            color="#a82f7a"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
- 
- 
- const styles = StyleSheet.create({
-   container: {
-         flex: 1,
-          justifyContent: 'center',  //na srodku
-          alignItems: 'center',   //tez
-          backgroundColor: 'powderblue',
-          flexDirection: 'column',
-   }});
-     
-     
- export default SecondScreen;
- 
+const musicTheory = {
+  dur: {
+    name: "DUR",
+    models: [
+      "Ces-dur",
+      "Ges-dur",
+      "Des-dur",
+      "As-dur",
+      "Es-dur",
+      "B-dur",
+      "F-dur",
+      "C-dur",
+      "G-dur",
+      "D-dur",
+      "A-dur",
+      "E-dur",
+      "H-dur",
+      "Fis-dur",
+      "Cis-dur",
+    ],
+  },
+  mol: {
+    name: "MOL",
+    models: [
+      "as-mol",
+      "es-mol",
+      "b-mol",
+      "f-mol",
+      "c-mol",
+      "g-mol",
+      "d-mol",
+      "a-mol",
+      "e-mol",
+      "h-mol",
+      "fis-mol",
+      "cis-mol",
+      "gis-mol",
+      "dis-mol",
+      "ais-mol",
+    ],
+  },
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f5f4",
+    flexDirection: "column",
+  },
+  elementContainer: {
+    marginTop: 8,
+  },
+  heading: {
+    fontSize: 22,
+    color: "black",
+  },
+});
+
+export default SecondScreen;
